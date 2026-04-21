4b90248cbfcf85c59c3f02456ca9fc9b3465c3f9
shivanshu7898 <sonishivanshu7898163335@gmail.com>
2026-04-21 20:04:40 +0530

    java script Start

---
+++ javaScript/script.js
@@ -0,0 +1,23 @@
+console.log("hello");
+
+let a = 5;
+
+var x = 10;
+
+
+let b = true;
+
+const  d = 10; 
+
+let p = 10;
+let q = '10';
+
+console.log(p==q);
+// true
+console.log(p===q); 
+// false 
+
+console.log(typeof q);
+
+
+console.log(a);
