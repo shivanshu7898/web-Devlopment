d633ebc0647a87011b1da5f7d20d0d8d569f2ea5
shivanshu7898 <sonishivanshu7898163335@gmail.com>
2026-04-29 19:25:23 +0530

    java script

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,23 +1,71 @@
-console.log("hello");
+// console.log("hello");
 
-let a = 5;
+// let a = 5;
 
-var x = 10;
-
-
-let b = true;
-
-const  d = 10; 
-
-let p = 10;
-let q = '10';
-
-console.log(p==q);
-// true
-console.log(p===q); 
-// false 
-
-console.log(typeof q);
+// var x = 10;
 
 
-console.log(a);
+// let b = true;
+
+// const  d = 10; 
+
+// let p = 10;
+// let q = '10';
+
+// console.log(p==q);
+// // true
+// console.log(p===q); 
+// // false 
+
+// console.log(typeof q);
+
+
+// console.log(a);
+
+
+
+// ..................loop...................
+
+// console.log("this is a for loop")
+
+// for(var i = 1; i<=10; i++){
+//     console.log(i*2);
+// }
+
+
+// console.log("first question")
+
+
+// let a = [20,55,56,44,77,66,11,77,33,777]
+
+// for(let i = 0; i<10; i++)
+// {
+//     console.log(a[i]);
+// }
+
+// let i =0;
+// while(i<10)
+// {
+//     console.log(a[i]);
+//     i++;
+// }
+
+
+// let i =1;
+// while(i<10)
+// {
+//     console.log(i*2);
+//     i++;
+// }
+
+
+let i = 1;
+do{
+
+    console.log(i*2);
+    i++;
+}while(i<10);
+
+
+
+
