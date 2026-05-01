db5704300051f4ddbaa3f49c65082fa7484bc34a
shivanshu7898 <sonishivanshu7898163335@gmail.com>
2026-05-01 19:13:03 +0530

    update script references and add conditional statements example

---
+++ javaScript/conditionalStatement.js
@@ -0,0 +1,80 @@
+// let a = 5;
+// if (a > 0) {
+//   console.log("positive");
+// } else {
+//   console.log("negative");
+// }
+// .........................
+// console.log("vote");
+// let age = 5;
+// if (a > 18) {
+//   console.log("you can vote");
+// } else {
+//   console.log("age " + age + " not eligible");
+// }
+// ....................
+// console.log("marks");
+// let a = 91;
+// if (a >= 90) {
+//   console.log("marks " + a + " Grade A");
+// } else if (a >= 90) {
+//   console.log("marks " + a + " Grade A");
+// } else if (a >= 75) {
+//   console.log("marks " + a + " Grade A");
+// } else if (a >= 50) {
+//   console.log("marks " + a + " Grade A");
+// } else {
+//   console.log("fail");
+// }
+// ...........................
+
+// let usr = "abcd";
+// let pass = "1234";
+
+// if (usr=="abcd" && pass=="1234") {
+//     console.log("success")
+
+// }
+// else(
+//     console.log("fail")
+// )
+
+// .....................
+
+// let data;
+// let name = "shiv";
+// data = name || "N/A";
+// console.log(data);
+
+
+
+// ...........Switch case..............
+
+
+
+// let choice;
+// switch(3){
+//     case 1:{
+//         console.log("tea");
+//         break;
+//     }
+//     case 2:{
+//         console.log("chai");
+//         break;
+//     }
+//     case 3:{
+//         console.log("pani");
+//         break;
+//     }
+//     case 3:{
+//         console.log("serbat");
+//         break;
+//     }
+//     case 4:{
+//         console.log("juice");
+//         break;
+//     }
+//     default:
+//         console.log("kuchh mtt pi tu")
+// }
+
---
+++ javaScript/mathod.js
@@ -0,0 +1,23 @@
+// let a = 10;
+// b = 20;
+// function sum(x, y) {
+//   let z = x + y;
+//   console.log(z);
+// }
+// sum(a, b);
+
+
+// function expression
+
+
+let abc = function()
+{
+    console.log("hello world");
+    
+}
+
+console.log(abc);
+
+abc();
+
+
