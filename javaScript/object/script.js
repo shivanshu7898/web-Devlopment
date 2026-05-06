7a635248212855bca45109d905330fdda8118658
shivanshu7898 <sonishivanshu7898163335@gmail.com>
2026-05-06 19:52:06 +0530

    feat: add initial HTML structure and JavaScript file with sample data

---
+++ javaScript/object/script.js
@@ -0,0 +1,60 @@
+// let fd = [
+//   ["shivanshu", "soni"],
+//   ["sataynshu", "gautam"],
+// ];
+
+// fd.forEach((element)=>{console.log(element);
+// })
+
+// JSON
+
+let people = [
+  {
+    name: "Amit Sharma",
+    work: "Software Engineer",
+    dob: "1998-05-12",
+    city: "Bhopal",
+    number: "9876543210",
+  },
+  {
+    name: "Priya Verma",
+    work: "Teacher",
+    dob: "1995-08-21",
+    city: "Indore",
+    number: "9123456780",
+  },
+  {
+    name: "Rahul Singh",
+    work: "Graphic Designer",
+    dob: "2000-02-10",
+    city: "Delhi",
+    number: "9988776655",
+  },
+  {
+    name: "Neha Gupta",
+    work: "Doctor",
+    dob: "1993-11-30",
+    city: "Mumbai",
+    number: "9012345678",
+  },
+  {
+    name: "Arjun Patel",
+    work: "Civil Engineer",
+    dob: "1997-07-15",
+    city: "Ahmedabad",
+    number: "9090909090",
+  },
+];
+
+// people.forEach((person) => {
+//   console.log(person);
+// });
+
+// console.log(people[1]["name"]);
+// console.log(people[1].name);
+
+console.log(Object.keys(people));
+console.log(Object.values(people));
+;
+
+
