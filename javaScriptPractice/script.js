5e3bce42ccce0b375ff00506f433ca65c93b03fc
shivanshu7898 <sonishivanshu7898163335@gmail.com>
2026-05-19 20:05:29 +0530

    event handling

--- javaScriptPractice/script.js
+++ javaScriptPractice/script.js
@@ -1,2 +1,14 @@
-const dulha = "haramjada";
-const dulhan = "soni";
+let p = document.querySelectorAll("p");
+
+
+p.forEach(function(item){
+    item.addEventListener("mouseover",()=>{
+        item.style.color= "red";
+    })
+    item.addEventListener("mouseout",()=>{
+        item.style.color= "";
+    })
+})
+
+
+
