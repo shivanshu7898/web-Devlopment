707a938f46e40b4d5d9161bc95db787dfbde3298
shivanshu7898 <sonishivanshu7898163335@gmail.com>
2026-07-27 20:16:20 +0530

    change

--- Backend/client/src/components/customerDashboard copy 3/CustomerOverview.jsx
+++ Backend/client/src/components/customerDashboard copy 3/CustomerOverview.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CustomerOverview = () => {
   return (
-    <div>CustomerOverview ka overview dikh </div>
+    <div>CustomerOverview ka overview dikh ---- dikhao </div>
   )
 }
 
